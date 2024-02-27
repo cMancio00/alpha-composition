8234d5d61eb4b68a8536826c5e0a5ad3fc39a52e
Christian Mancini <mancinichristian00@gmail.com>
2024-02-27 10:17:46 +0100

    Fixed an issue that causes 3 channels images to not be saved correctly

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 #include <iostream>
+#include <omp.h>
 
-#define FOREGROUND_PATH "../Input/pooh.png"
-#define BACKGROUND_PATH "../Input/mario.png"
-
+#define FOREGROUND_PATH "../Input/pooh2k.png"
+#define BACKGROUND_PATH "../Input/1.jpg"
 #define OUTPUT_PATH "../Output/output.png"
 
 struct Image{
@@ -15,7 +15,6 @@ struct Image{
 };
 
 void loadImage(const std::string &image_path, Image &image){
-    //TODO: usare stbi_info per leggere il numero di canali
     image.rgb_image = stbi_load(image_path.c_str(),&image.width, &image.height,
                                      &image.channels, STBI_rgb_alpha);
     if(!image.rgb_image){
@@ -25,14 +24,16 @@ void loadImage(const std::string &image_path, Image &image){
         std::cout << image.width<<"x"<< image.height<< " and "
                   << image.channels << " channels." <<std::endl;
         if(image.channels != STBI_rgb_alpha){
-            std::cout << "WARNING: Image does not have alpha channel!" << std::endl;
+            std::cout << "WARNING: Image does not have alpha channel!" << std::endl
+            << "Forcing alpha channel to 255 (completely visible)." << std::endl;
+            image.channels = STBI_rgb_alpha;
         }
     }
 }
 
 void saveImage(const std::string &output_path, const Image image){
     stbi_write_png(output_path.c_str(), image.width, image.height, image.channels,
-                   image.rgb_image, image.width * image.channels);
+                   image.rgb_image, image.width * image.channels );
 }
 
 int main(){
@@ -41,35 +42,37 @@ int main(){
     Image background;
     loadImage(BACKGROUND_PATH,background);
 
+    double startTime = omp_get_wtime();
 
     for(int y = 0; y < foreground.height; ++y){
         for(int x = 0; x < foreground.width; ++x){
 
-            int backgroundIndex = (y * background.width + x) * background.channels;
-            int foregroundIndex = (y * foreground.width + x) * foreground.channels;
+            int backgroundIndex = (y * background.width + x) * STBI_rgb_alpha;
+            int foregroundIndex = (y * foreground.width + x) * STBI_rgb_alpha;
 
             float alpha = foreground.rgb_image[foregroundIndex + 3] / 255.0f;
             float beta = 1.0f - alpha;
 
-            for (int c = 0; c < 3; ++c) {
-                background.rgb_image[backgroundIndex + c] =
-                        static_cast<int>(background.rgb_image[backgroundIndex + c] * beta
-                        + foreground.rgb_image[foregroundIndex + c] * alpha);
+            for (int color = 0; color < 3; ++color) {
+                background.rgb_image[backgroundIndex + color] =
+                        background.rgb_image[backgroundIndex + color] * beta
+                        + foreground.rgb_image[foregroundIndex + color] * alpha;
 
             }
         }
     }
 
+    double endTime = omp_get_wtime();
+    std::cout << "Compositing time: " << endTime - startTime << std::endl << std::endl;
 
-    std::cout << "Done" << std::endl;
-
+    std::cout << "Saving output image as "<< OUTPUT_PATH << std::endl;
     saveImage(OUTPUT_PATH,background);
-    std::cout << "Saved" << std::endl;
+    std::cout << "Output image saved" << std::endl;
 
+    std::cout << "Releasing memory resources" << std::endl;
     stbi_image_free(foreground.rgb_image);
     stbi_image_free(background.rgb_image);
-
-
+    std::cout << "Done" << std::endl;
 
     return 0;
 }
